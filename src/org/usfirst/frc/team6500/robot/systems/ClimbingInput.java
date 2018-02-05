f9255493f321d7ebb17a033b01d60d11fec36535
AndrewLopez <AndrewLopez@10.87.18.73>
2018-02-05 17:19:50 -0500

    Motor Climbing Class and motor ports for motors

--- src/org/usfirst/frc/team6500/robot/systems/ClimbingInput.java
+++ src/org/usfirst/frc/team6500/robot/systems/ClimbingInput.java
@@ -2,4 +2,14 @@ package org.usfirst.frc.team6500.robot.systems;
 
 public class ClimbingInput {
 
+	public double speed;
+	public boolean directionUp;
+	public boolean directionDown;
+	
+	public static void initializeClimbMotor(){
+		
+		
+		
+	}
+	
 }
