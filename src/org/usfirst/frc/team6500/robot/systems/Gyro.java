9d9829ceefc4624b4a08a4521ebd718dddadbdac
twdea <twdea@10.87.16.246>
2018-02-18 11:26:41 -0500

    general merge

---
+++ src/org/usfirst/frc/team6500/robot/systems/Gyro.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team6500.robot.systems;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+public class Gyro {
+	static ADXRS450_Gyro gyro;
+	
+	public static void intializeGyro()
+	{
+		gyro = new ADXRS450_Gyro();
+	}
+	
+	public static double getAngle()
+	{
+		return gyro.getAngle();
+	}
+
+	public static void reset() {
+		gyro.reset();
+	}
+}
