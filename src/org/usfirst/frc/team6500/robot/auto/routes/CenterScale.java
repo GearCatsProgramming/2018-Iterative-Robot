6c623d1cf8d27ebd031044b3a3fbf54baf19363a
twdea <twdea@10.87.28.4>
2018-04-06 11:02:06 -0400

    IET:JFAIODAKLPJFMIO:ESAJSSIODCFKJWEOILSD

---
+++ src/org/usfirst/frc/team6500/robot/auto/routes/CenterScale.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team6500.robot.auto.routes;
+
+import org.usfirst.frc.team6500.robot.Robot;
+import org.usfirst.frc.team6500.robot.auto.AutoRoute;
+import org.usfirst.frc.team6500.robot.auto.AutoWrapper;
+
+public class CenterScale implements AutoRoute {
+	private static double inches0 = 100;
+	
+	private static double degrees0 = 90;
+	
+	private static final double fieldLength = 299.5;
+	
+	private boolean done;
+	private Robot robot;
+	private double speed;
+	
+	private boolean left;
+	
+	public CenterScale(double speed, boolean left, Robot robot) {
+		if(left) { inches0 *= -1; degrees0 *= -1; };
+		
+		this.speed = speed;
+		this.robot = robot;
+		this.done = false;
+		this.left = left;
+	}
+	
+	@Override
+	public void run() {
+		AutoWrapper.leftRight(inches0, speed, this.robot);
+		ForwardScale thing = new ForwardScale(speed, left, robot);
+		while(!thing.isDone()) {}
+	}
+
+	@Override
+	public boolean isDone() {return this.done;}
+
+}
