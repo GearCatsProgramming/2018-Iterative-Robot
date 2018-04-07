f4c2afd5e36e46669dcc0fefb9eadb2f51ed6ed7
twdea <twdea@DESKTOP-AG3VRI4>
2018-04-07 13:54:05 -0400

    Crappy refactor

--- src/org/usfirst/frc/team6500/robot/Position.java
+++ src/org/usfirst/frc/team6500/robot/Position.java
@@ -1,5 +1,19 @@
 package org.usfirst.frc.team6500.robot;
 
+/**An enum listing where objects might be
+ * @author Thomas Dearth
+ * @since intricate stuff might be within our reach :O
+ */
 public enum Position {
-	left, middle, right
+	left, middle, right;
+
+	/**Converts a {@link Position} to a boolean
+	 * @param side The position requested
+	 * @return true if left, false otherwise
+	 */
+	public static boolean toBoolean(Position side) {
+		if(side == Position.left) { 		return true; }
+		else if(side == Position.right) { 	return false; }
+		else { 								return false; }
+	}
 }
--- src/org/usfirst/frc/team6500/robot/auto/routes/RouteMap2.java
+++ src/org/usfirst/frc/team6500/robot/auto/routes/RouteMap2.java
@@ -11,6 +11,16 @@ import org.usfirst.frc.team6500.robot.auto.AutoRoute;
  */
 public class RouteMap2 
 {
+	/**Gets the desired route. Maybe.
+	 * @author Thomas Dearth
+	 * @param autoSpeed The speed the route is run at.
+	 * @param robot The robot doing it. WHY U NO STATIC
+	 * @param start The starting position {@link Position}
+	 * @param switchLoc The position of the friendly side of the switch {@link Position}
+	 * @param scaleLoc The position of the friendly side of the scale {@link Position}
+	 * @param priority The thing we'd rather put cubebs on {@link Target}
+	 * @return The route we're going to do.
+	 */
 	public static AutoRoute getRoute(double autoSpeed, Robot robot, Position start, Position switchLoc, Position scaleLoc, Target priority) {
 		//If everything is nonfunctional, die a bit inside
 		if(priority == Target.hitAutoLine) { return new AutoLine(autoSpeed, robot); }
@@ -18,27 +28,21 @@ public class RouteMap2
 		//If we're in the middle, check to see if we want the scale or the switch, and do that
 		if(start == Position.middle) {
 			if(priority == Target.hitSwitch) {
-				return new MiddleSwitch(autoSpeed, toBoolean(switchLoc), robot);
+				return new MiddleSwitch(autoSpeed, Position.toBoolean(switchLoc), robot);
 			} else {
-				return new CenterScale(autoSpeed, toBoolean(scaleLoc), robot);
+				return new CenterScale(autoSpeed, Position.toBoolean(scaleLoc), robot);
 			}
 		}
 		
 		//If we're not on the middle, check the priority, then if that's on the far side, do the other thing
 		if(priority == Target.hitSwitch) {
-			if(start == switchLoc) {			return new ForwardSwitch(autoSpeed, toBoolean(start), robot);
-			} else if(start == scaleLoc) {		return new ForwardScale(autoSpeed, toBoolean(start), robot);
-			} else {							return new OppositeSwitch(autoSpeed, toBoolean(start), robot); }
+			if(start == switchLoc) {			return new ForwardSwitch(autoSpeed, Position.toBoolean(start), robot);
+			} else if(start == scaleLoc) {		return new ForwardScale(autoSpeed, Position.toBoolean(start), robot);
+			} else {							return new OppositeSwitch(autoSpeed, Position.toBoolean(start), robot); }
 		} else {
-			if(start == scaleLoc) { 			return new ForwardScale(autoSpeed, toBoolean(start), robot);
-			} else if(start == switchLoc) { 	return new ForwardSwitch(autoSpeed, toBoolean(start), robot);
-			} else { 							return new OppositeScale(autoSpeed, toBoolean(start), robot); }
+			if(start == scaleLoc) { 			return new ForwardScale(autoSpeed, Position.toBoolean(start), robot);
+			} else if(start == switchLoc) { 	return new ForwardSwitch(autoSpeed, Position.toBoolean(start), robot);
+			} else { 							return new OppositeScale(autoSpeed, Position.toBoolean(start), robot); }
 		}
-	}
-	
-	public static boolean toBoolean(Position side) {
-		if(side == Position.left) { 		return true; }
-		else if(side == Position.right) { 	return false; }
-		else { 								return false; }
 	}
 }
